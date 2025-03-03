322f3d8ad11c1eeefcc79a7f0b98141d8cb112f5
Syed Masood Hussain <hmasood3288@gmail.com>
2025-03-03 06:19:27 +0500

    Add image to background, draw bird, and Timer Loop.

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,2 +1,85 @@
-public class FlappyBird {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel implements ActionListener{
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    // Images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    // Bird
+    int BirdX = boardWidth/8;
+    int BirdY = boardHeight/2;
+    int BirdWidth = 34;
+    int BirdHeight = 24;
+
+
+    class Bird{
+        int x = BirdX;
+        int y = BirdY;
+        int width = BirdWidth;
+        int height = BirdHeight;
+        Image img;
+
+        Bird(Image img){
+            this.img = img;
+        }
+    }
+
+//    GAME LOGIC
+    Bird bird;
+    int velocityY = -6;
+
+//    TIMER LOOP
+    Timer gameLoop;
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+//        setBackground(Color.blue);
+
+        // Load Images
+        backgroundImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/flappybirdbg.png"))).getImage();
+        birdImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/flappybird.png"))).getImage();
+        topPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/toppipe.png"))).getImage();
+        bottomPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/bottompipe.png"))).getImage();
+
+//        Bird Object Initialization
+        bird = new Bird(birdImg);
+
+//        Game Timer
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        // Background
+        g.drawImage(backgroundImg, 0,0, boardWidth, boardHeight, null);
+
+//        Drawing Bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+    public void move(){
+        // Bird
+        bird.y += velocityY;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,18 @@ public class Main {
         int boardwidth = 360;
         int boardheight = 640;
 
+        // Setting up the window
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(boardwidth, boardheight);
+
+        // Creating Instance of FlappyBird
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack(); // Added this so that Panel couldn't take the title bar size too.
+        frame.setVisible(true);
+
     }
 }
